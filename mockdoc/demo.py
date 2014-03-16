ffffefaf6ed8b1d237e4bb516114e39632264a30
Michael Hrivnak <mhrivnak@redhat.com>
2014-03-16 18:36:48 -0400

    first commit where it actually works

--- mockdoc/demo.py
+++ mockdoc/demo.py
@@ -1,18 +1,24 @@
 import mockdoc
-import mock
+from mock import Mock
+
 
 def bar(x, y):
     """
     :type x:    int
-    :type y:    int
+    :type y:    mock.Mock
     """
     print x, y
 
 
+class Crap(object):
+    pass
+
 @mockdoc.patch('__main__.bar', autospec=True)
 def stuff(mock_bar):
-    bar(1, 2)
-    mock_bar.assert_called_once_with(1, 2)
+    c = Mock()
+    bar(1, c)
+    mock_bar.assert_called_once_with(1, c)
 
 
 stuff()
+
--- mockdoc/mockdoc.py
+++ mockdoc/mockdoc.py
@@ -1,24 +1,53 @@
+import inspect
 import re
 
 import mock
 
-def patch(*args, **kwargs):
 
+arg_pattern = re.compile('^:type\s+(\w+):\s+([^\s]+)$')
+return_pattern = re.compile('^:rtype:\s+([^\s]+)$')
+
+
+def patch(*args, **kwargs):
     def _dec(f):
         patcher = mock.patch(*args, **kwargs)
         with patcher:
-            docblock = patcher.temp_original.__doc__
+            original = patcher.temp_original
+
+        docblock = inspect.getdoc(original)
         
-        def foo(mock_something, *args, **kwargs):
+        def replacement(mock_something, *args, **kwargs):
             ret = f(mock_something, *args, **kwargs)
-            validate_calls(docblock, mock_something.call_args)
+            validate_calls(original, docblock, mock_something.call_args_list)
             return ret
             
-        mock_obj = mock.patch(*args, **kwargs)(foo)
+        mock_obj = mock.patch(*args, **kwargs)(replacement)
         return mock_obj
 
     return _dec
 
-def validate_calls(docblock, calls):
-    print docblock
-    print calls
+
+def validate_calls(orig_func, docblock, calls):
+    for call in calls:
+        args = call[0]
+        kwargs = call[1]
+        arg_map = inspect.getcallargs(orig_func, *args, **kwargs)
+        for line in docblock.split('\n'):
+            match = arg_pattern.match(line)
+            if match:
+                name, expected_type_name = match.groups()
+                expected_type = get_type(expected_type_name, orig_func)
+                if expected_type is None:
+                    raise ValueError('documented type [%s] could not be found' % expected_type_name)
+                if not isinstance(arg_map[name], expected_type):
+                    raise TypeError('mock called with wrong type for arg [%s]' % name)
+
+
+def get_type(name, orig_func):
+    if name in __builtins__:
+        builtin = __builtins__[name]
+        if isinstance(builtin, type):
+            return builtin
+
+    return mock._importer(name)
+
